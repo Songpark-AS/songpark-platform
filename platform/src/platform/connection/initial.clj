3af76746a174e0eaad8b528f3f244fd2d3b34bb1
Sindre Engøy <sindre.engoy@inonit.no>
2021-07-19 17:23:31 +0200

    makes initial connection code easier to read

--- platform/src/platform/connection/initial.clj
+++ platform/src/platform/connection/initial.clj
@@ -3,19 +3,18 @@
             [taoensso.timbre :as log]
             [songpark.taxonomy.auth]))
 
+(defn- basic-return-message [& items] 
+  {:status 200
+   :body (apply str items)})
 
 (defn tp-handler [input]
-  (let [sub (:params input)] 
-  (if (get sub "tp-id")
-    {:status 200
-     :body (str "You are tp. Your tp-id is: " (get sub "tp-id"))}
-    {:status 200
-     :body (str "You are tp. You must provide tp-id")}))
-)
+  (let [params (:params input)] 
+  (if (get params "tp-id")
+    (basic-return-message "You are tp. Your tp-id is: " (get params "tp-id"))
+    (basic-return-message "You are tp. You must provide tp-id"))))
+
 (defn client-handler [input]
-  (let [sub (:params input)]
-    (if (get sub "tp-id")
-    {:status 200
-     :body (str "You are client. You wish to connect to tp-id: " (get sub "tp-id"))}
-    {:status 200
-     :body (str "You are client. You must provide tp-id")})))
+  (let [params (:params input)]
+    (if (get params "tp-id")
+      (basic-return-message "You are client. You wish to connect to tp-id: " (get params "tp-id"))
+      (basic-return-message "You are client. You must provide tp-id"))))
