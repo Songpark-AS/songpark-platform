178093b72b9163d9a6d6f66e836ccf2c4b05bc9a
Sindre Engøy <sindre.engoy@inonit.no>
2021-07-22 13:34:05 +0200

    fixes column name typo

--- platform/resources/migrations/20210722104804-add-columns-for-tp-table.down.sql
+++ platform/resources/migrations/20210722104804-add-columns-for-tp-table.down.sql
@@ -2,5 +2,5 @@ ALTER TABLE IF EXISTS tp
 ADD IF NOT EXISTS active boolean,
 DROP IF EXISTS on_status,
 DROP IF EXISTS available_status,
-DROP IF EXISTS nick_name,
+DROP IF EXISTS nickname,
 DROP IF EXISTS unique_id;
--- platform/resources/migrations/20210722104804-add-columns-for-tp-table.up.sql
+++ platform/resources/migrations/20210722104804-add-columns-for-tp-table.up.sql
@@ -2,5 +2,5 @@ ALTER TABLE IF EXISTS tp
 DROP IF EXISTS active,
 ADD IF NOT EXISTS on_status boolean,
 ADD IF NOT EXISTS available_status boolean,
-ADD IF NOT EXISTS nick_name text,
+ADD IF NOT EXISTS nickname text,
 ADD IF NOT EXISTS unique_id text;
