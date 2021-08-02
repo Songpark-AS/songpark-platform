f265473d8480c80a958c40b04b0e39471583e171
Sindre Engøy <sindre.engoy@inonit.no>
2021-08-02 16:49:26 +0200

    adds common-code to fakephone

--- utility/fakephone/src/fakephone/core.clj
+++ utility/fakephone/src/fakephone/core.clj
@@ -2,18 +2,18 @@
   (:require [clojure.tools.cli :refer [parse-opts] :as cli]
             [org.httpkit.client :as http]
             [clojure.edn :as edn]
-            [clojure.data.json :as json])
+            [clojure.data.json :as json]
+            [common.connect.client :as connect.client])
   (:gen-class))
 
 (def cli-options
   [["-c" "--connect-to-endpoint ENDPOINT" "The address of the endpoint resource you are trying to access"
-   :id :connect-to-endpoint
-   :default "http://localhost:3000/connect/client/init"]
+    :id :connect-to-endpoint
+    :default "http://localhost:3000/connect/client/init"]
    ["-n" "--nickname NICKNAME" "The nickname of the teleporter you are trying to connect to"
     :id :nickname]
    ["-h" "--help" "Displays this screen"
-    :id :help]]
-  )
+    :id :help]])
 
 (defn -main
   [& cli-args]
@@ -23,23 +23,16 @@
         help (get-in input [:options :help])]
     (if help
       (println (:summary input))
-      (let [response (try
-                       @(http/get endpoint {:query-params {:nickname nickname}}))
-            response-body (:body response)
-            respond-params (if response-body
-                             (read-string (get (json/read-str (:body response)) "value"))
-                             nil)
+      (let [respond-params (connect.client/init {:nickname nickname})
             status (:status respond-params)
+            status-desc (:status-desc respond-params)
             username (:mqtt-username respond-params)
             password (:mqtt-password respond-params)]
         (println "Connecting to endpoint:" endpoint)
         (case status
           nil (println "ERROR: No status retrieved. Possibly bad endpoint")
-          "ERROR-no-nickname" (println "ERROR: You must provide tp-nickname")
-          "ERROR-tp-unavailable" (println "ERROR: Tp can not be accessed")
           "success" (do
                       (println "SUCCESS: Tp is available, with the following MQTT credentials:")
                       (println "MQTT-username: " username)
-                      (println "MQTT-password: " password))))))) 
-
-
+                      (println "MQTT-password: " password))
+          status-desc)))))
