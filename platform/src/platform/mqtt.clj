15a47ff1903d46d620bd9d0a1fca6bb125c6535d
Daniel W. D <daniel.delatre@inonit.no>
2021-09-29 14:31:43 +0200

    Rename on-receice -> on-message

--- platform/src/platform/mqtt.clj
+++ platform/src/platform/mqtt.clj
@@ -15,6 +15,7 @@
   (let [out (ByteArrayOutputStream. 4096)]
     (transit/write (transit/writer out :json {:handlers write-handlers}) v)
     (.toString out "utf-8")))
+
 (defn- <-transit [b]
   (try
     (transit/read (transit/reader (ByteArrayInputStream. b) :json))
@@ -22,15 +23,15 @@
                            (apply str (map char b))))))
 
 (defn- subscribe* [{:keys [client] :as mqtt-manager} topics]
-  (letfn [(on-receive [^String topic _ ^bytes payload]            
+  (letfn [(on-message [^String topic _ ^bytes payload]            
             (->> (merge (<-transit payload) {:message/topic topic})
                  handle-message))]
-    (.subscribe client topics on-receive)))
+    (.subscribe client topics on-message)))
 
 (defn- unsubscribe* [{:keys [client] :as mqtt-manager} topics]
   (.unsubscribe client topics))
 
-(defn- publish!* [{:keys [client] :as mqtt-manager} topic msg]
+(defn- publish* [{:keys [client] :as mqtt-manager} topic msg]
   (.publish client topic (->transit msg)))
 
 (defrecord MQTTManager [injection-ks started? config]
@@ -57,8 +58,15 @@
     (subscribe* this topics))
   (unsubscribe [this topics]
     (unsubscribe* this topics))
-  (publish! [this topic msg]
-    (publish!* this topic msg)))
+  (publish [this topic msg]
+    (publish* this topic msg)))
 
 (defn mqtt-manager [settings]
   (map->MQTTManager settings))
+
+
+(comment
+
+
+  
+  )
