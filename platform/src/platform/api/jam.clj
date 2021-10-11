0cf7c8aef6dfdb11a317b1ee73c775fe9ccdee78
Daniel W. D <daniel.delatre@inonit.no>
2021-10-11 13:09:47 +0200

    Fix stop jam function

--- platform/src/platform/api/jam.clj
+++ platform/src/platform/api/jam.clj
@@ -22,7 +22,7 @@
 ;; a response to the frontend with jam/uuid, which will be used as a jam topic
 ;; on mqtt... LATERZ...
 
-(defn connect [{:keys [data parameters]}]
+(defn start [{:keys [data parameters]}]
   (let [uuids (-> parameters :body)
         jam-id (uuid/v4)]
     (db/wr [:jam jam-id] (->> uuids (map :teleporter/uuid)))
@@ -40,11 +40,14 @@
       {:status 400
        :body {:error/message "Invalid: No members in jam"}})))
 
-(defn disconnect [{:keys [data parameters]}]
-  (let [jam-id (-> parameters :body :jam/uuid)]
-    (db/wr [:jam] dissoc jam-id)
-    {:status 200
-     :body ""}))
+(defn stop [{:keys [data parameters]}]
+  (let [jam-id (-> parameters :body :jam/uuid)
+        res (->> (db/wr [:jam] dissoc jam-id) :jam keys (filter #{jam-id}))]        
+    (if-not (empty? res)
+      {:status 200
+       :body ""}
+      {:status 400
+       :body {:error/message "Could not stop jam"}})))
 
 
 
@@ -52,5 +55,9 @@
   (let [jam-id (first (keys (db/rd [:jam])))]
     (keys (jam-topics jam-id))
     )
-  
+
+  (db/rd [:teleporter])
+  (db/rd [:jam])
+
   )
+
