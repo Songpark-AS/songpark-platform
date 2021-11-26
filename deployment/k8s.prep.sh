ab2cbb65970abb3248ca0fa443f2ea872c42c4aa
Mathias Kjeldsberg Andersen <hemanngosser@gmail.com>
2021-11-26 15:14:03 +0100

    Add namespace and hostname variables to songpark-platform.template.yaml, change sed commands in k8s.prep.sh to work with the GNU version of sed

--- deployment/k8s.prep.sh
+++ deployment/k8s.prep.sh
@@ -5,10 +5,15 @@ TEMPLATE_FILE=songpark-platform.template.yaml
 YAML_FILE=songpark-platform.yaml
 TAG=$1
 VERSION=$2
+NAMESPACE=songpark-$TAG
+HOSTNAME=spp-$TAG.inonit.no
 
 echo "Copying template file"
 cp $DEPLOYMENT_DIR/$TEMPLATE_FILE $DEPLOYMENT_DIR/$YAML_FILE
 
 echo "Updating variables"
-sed -i '' 's/VAR__VERSION/'"$VERSION"'/g' $DEPLOYMENT_DIR/$YAML_FILE
-sed -i '' 's/VAR__TAG/'"$TAG"'/g' $DEPLOYMENT_DIR/$YAML_FILE
+sed -i -e 's/VAR__VERSION/'"$VERSION"'/g' $DEPLOYMENT_DIR/$YAML_FILE
+sed -i -e 's/VAR__TAG/'"$TAG"'/g' $DEPLOYMENT_DIR/$YAML_FILE
+sed -i -e 's/VAR__NAMESPACE/'"$NAMESPACE"'/g' $DEPLOYMENT_DIR/$YAML_FILE
+sed -i -e 's/VAR__HOSTNAME/'"$HOSTNAME"'/g' $DEPLOYMENT_DIR/$YAML_FILE
+
