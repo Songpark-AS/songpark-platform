b6b19febfbeb9b2ec65717ab3840b69a242501a7
Emil Bengtsson <emil@emil0r.com>
2022-03-02 19:10:57 +0100

    Remove message
    Removed message and the mess it represented.

--- platform/src/platform/message/dispatch.clj
+++
@@ -1,9 +0,0 @@
-(ns platform.message.dispatch
-  (:require [platform.message.dispatch.platform]
-            [platform.message.dispatch.teleporter]
-            [platform.message.dispatch.jam]
-            [platform.message.dispatch.interface :as interface]))
-
-
-(defn handler [msg]
-  (interface/dispatch msg))
