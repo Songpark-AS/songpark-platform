e5f2424a775020bcfb408cdf57c48f2dc57b56c0
Emil Bengtsson <emil@emil0r.com>
2022-05-06 13:44:51 +0200

    Add updated_at trigger function
    Base tables should hold created_at and updated_at columns for basic
    SQL database hygiene. This is the generic function that the triggers use.

--- platform/dev/migrations.clj
+++ platform/dev/migrations.clj
@@ -4,15 +4,15 @@
             [migratus.core :as migratus]))
 
 (defn migratus-config []
-  (let [ds (get-in @init/system [:database :db-specs :default :datasource])
-        mmap (get-migration-map ds)]
+  (let [ezdb (get-in @init/system [:database])
+        ds (get-in @init/system [:database :db-specs :default :datasource])
+        mmap (get-migration-map ezdb ds)]
     (assert (some? ds) "The system needs to be initialized in order to work with migrations")
     mmap))
 
 (comment
 
-  (migratus/create (migratus-config) "create-quint-table")
-  (migratus/create (migratus-config) "view-student-history-added-locale")
+  (migratus/create (migratus-config) "add-preset")
   (migratus/migrate (migratus-config))
   (migratus/rollback (migratus-config))
   )
