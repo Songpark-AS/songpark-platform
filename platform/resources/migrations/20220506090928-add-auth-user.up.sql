ec990c0c746ff082c4ac31ce5297bcef9e63fc89
Emil Bengtsson <emil@emil0r.com>
2022-05-06 14:18:39 +0200

    Add auth_user to the database
    Holds the standard stuff for a user SQL table. It is currently only
    based on the standard email/password authentication process to
    authenticate. No authorization is as of yet present in the solution.

---
+++ platform/resources/migrations/20220506090928-add-auth-user.up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE auth_user (
+  id serial primary key,
+  created_at timestamp default now(),
+  updated_at timestamp default now(),
+  email text,
+  password text not null
+
+);
+--;;
+CREATE TRIGGER auth_user_trigger_updated_at
+  BEFORE UPDATE
+  ON auth_user
+  FOR EACH ROW
+    EXECUTE PROCEDURE updated_at_trigger_func();
