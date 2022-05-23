8b60cfdc995cd08dcf9ade5085f0e2cdfde6187d
Emil Bengtsson <emil@emil0r.com>
2022-05-23 21:38:35 +0200

    Update profile migration
    Changed user_id and pronoun_id to be not null. Added a default value
    of profile_pronoun as -1, ''.

--- platform/resources/migrations/20220506144053-add-profile.up.sql
+++ platform/resources/migrations/20220506144053-add-profile.up.sql
@@ -3,16 +3,18 @@ CREATE TABLE profile_pronoun (
   name text not null
 );
 --;;
+INSERT INTO profile_pronoun VALUES (-1, '');
+--;;
 CREATE TABLE profile_profile (
   id serial primary key,
   created_at timestamp default now(),
   updated_at timestamp default now(),
-  user_id integer references auth_user(id),
+  user_id integer references auth_user(id) not null,
   name text not null,
   location text not null,
   bio text not null,
   image_url text not null,
-  pronoun_id integer references profile_pronoun(id)
+  pronoun_id integer references profile_pronoun(id) not null default -1
 );
 --;;
 CREATE TRIGGER profile_profile_trigger_updated_at
