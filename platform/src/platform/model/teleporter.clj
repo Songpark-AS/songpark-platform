8d6c7cb3d317457c49b4fe97e3f63b78b606e587
Emil Bengtsson <emil@emil0r.com>
2022-09-17 16:10:43 +0200

    Fix save-settings in teleporter
    If you had more than one teleporter in your settings (from previous
    pairings), the code sent back the first one, instead of the one you
    had just updated.

--- platform/src/platform/model/teleporter.clj
+++ platform/src/platform/model/teleporter.clj
@@ -8,22 +8,24 @@
                [:teleporter_id :teleporter/id]
                [:name          :teleporter/nickname])
 
-(defn get-settings [db user-id]
+(defn get-settings [db user-id teleporter-id]
   (->> {:select [:*]
         :from [:teleporter_settings]
-        :where [:= :user_id user-id]}
+        :where [:and
+                [:= :user_id user-id]
+                [:= :teleporter_id teleporter-id]]}
        (db/query db ^:opts {[:transformation :post]
                             [:setting :teleporter/setting]})
        first))
 
 (defn save-settings [db user-id data]
-  (let [result (db/query! db {:update :teleporter_settings
+  (let [result #dbg (db/query! db {:update :teleporter_settings
                               :set {:name (:teleporter/nickname data)}
                               :where [:and
                                       [:= :teleporter_id (:teleporter/id data)]
                                       [:= :user_id user-id]]})]
     (if-not (empty? result)
-      (get-settings db user-id)
+      (get-settings db user-id (:teleporter/id data))
       false)))
 
 
